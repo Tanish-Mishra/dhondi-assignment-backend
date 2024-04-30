2a64debe3f23ad0fe37a4c247a41308fed97c576
Tanish Mishra <tanishmishra97@gmail.com>
2024-05-01 00:04:23 +0530

    feat: edit blog by id api added

--- controllers/blogController.js
+++ controllers/blogController.js
@@ -56,9 +56,35 @@ const deleteBlog = async(req,res,next) => {
     }
 }
 
+const editBlogById = async(req,res,next) => {
+    try {
+        const id = req.params.id
+        const { blogTitle, blogDescription, keywords } = req.body;
+        
+        if(!id || !blogTitle || !blogDescription || !keywords) {
+            res.status(400).json({
+                message: "Please provide id,title,description and keywords"
+            })
+        }
+        
+        await Blogs.findByIdAndUpdate(id,{
+            blogTitle,
+            blogDescription,
+            keywords
+        })
+        
+        res.status(200).json({
+            message: "Blog Updated Successfully!"
+        })
+    } catch (error) {
+        next(error)
+    }
+
+}
 
 module.exports = {
   getAllBlogs,
   addBlog,
   deleteBlog,
+  editBlogById
 };
--- routes/blogRoutes.js
+++ routes/blogRoutes.js
@@ -1,11 +1,12 @@
 const express = require('express')
 const Router = express.Router() 
 
-const {getAllBlogs,addBlog,deleteBlog} = require('../controllers/blogController')
+const {getAllBlogs,addBlog,deleteBlog,editBlogById} = require('../controllers/blogController')
 
 Router.get('/get-blog',getAllBlogs)
 Router.post('/create-blog',addBlog)
 Router.delete('/delete-blog/:id',deleteBlog)
+Router.patch('/edit-blog/:id',editBlogById)
 
 
 module.exports = Router
